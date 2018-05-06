6c680e08c8dd46f6c089ef4ee53015f350f5fe43
tom <1297320697@qq.com>
2018-05-06 23:30:58 +0800

    first commit

---
+++ src/main/java/com/mapgis/service/UserService.java
@@ -0,0 +1,24 @@
+package com.mapgis.service;
+
+import com.mapgis.dao.UserRepositoty;
+import com.mapgis.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Song on 2017/2/15.
+ * User业务逻辑
+ */
+@Service
+public class UserService {
+    @Autowired
+    private UserRepositoty userRepositoty;
+
+    public User findUserByName(String name){
+        User user = null;
+        try{
+            user = userRepositoty.findByUserName(name);
+        }catch (Exception e){}
+        return user;
+    }
+}
