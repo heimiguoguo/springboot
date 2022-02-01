ef9b3d2531d0daf08bf0e181f658c56662c2155a
Tom <1297320697@qq.com>
2022-02-01 14:00:20 +0800

    .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # springboot
 用idea搭建一个springboot项目,随着学习深入会不断往里边添加新的东西，并记录学习中遇到的一些问题以及解决方案
+
+
+# idea快速创建main方法
+    psvm
+    
+# SpringBoot项目git ignore target等目录
+    [https://www.renyiwei.com/archives/1590.html](https://www.renyiwei.com/archives/1590.html)
---
+++ src/main/java/com/mapgis/MySpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.mapgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Tom on 2022/2/1.
+ */
+@SpringBootApplication
+public class MySpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+}
